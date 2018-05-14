2ad323e64e3282bd083302307485cce6810fd711
smith.hu <smith.hu@cyweemotion.com>
2018-05-14 18:53:53 +0800

    [add] sensorFind, simulate

--- sensors_manager/contexthub/firmware/inc/sensors.h
+++ sensors_manager/contexthub/firmware/inc/sensors.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
 
 #include <stdint.h>
+#include <stdbool.h>
 
 struct SensorInfo {
   const char *sensorName; /* sensors.c code does not use this */
@@ -38,5 +39,28 @@ struct SensorInfo {
   uint8_t rawType;
   float rawScale;
 };
+
+/*
+ * Sensor rate is encoded as a 32-bit integer as number of samples it can
+ * provide per 1024 seconds, allowing representations of all useful values
+ * well. This define is to be used for static values only please, as GCC
+ * will convert it into a const int at compile time. Do not use this at
+ * runtime please. A few Magic values exist at both ends of the range
+ * 0 is used as a list sentinel and high numbers for special abilities.
+ */
+#define SENSOR_RATE_ONDEMAND    0xFFFFFF00UL
+#define SENSOR_RATE_ONCHANGE    0xFFFFFF01UL
+#define SENSOR_RATE_ONESHOT     0xFFFFFF02UL
+#define SENSOR_HZ(_hz)          ((uint32_t)((_hz) * 1024.0f))
+
+/*
+ * api for using sensors (enum is not synced with sensor sub/unsub, this is ok since we do not expect a lot of dynamic sub/unsub)
+ */
+const struct SensorInfo* sensorFind(uint32_t sensorType, uint32_t idx, uint32_t *handleP); //enumerate all sensors of a type
+bool sensorRequest(uint32_t clientTid, uint32_t sensorHandle, uint32_t rate, uint64_t latency);
+bool sensorRelease(uint32_t clientTid, uint32_t sensorHandle);
+
+#define EVT_NO_FIRST_SENSOR_EVENT        0x00000200    //sensor type SENSOR_TYPE_x produces events of type EVT_NO_FIRST_SENSOR_EVENT + SENSOR_TYPE_x for all Google-defined sensors
+#define sensorGetMyEventType(_sensorType) (EVT_NO_FIRST_SENSOR_EVENT + (_sensorType))
 
 #endif
